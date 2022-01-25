631fa8b4d0d983b55d2dfb9b29a05896d6d28418
sina shariati <sina.shariati@yahoo.com>
2022-01-25 14:21:57 +0330

    Finish dijkstra algorithm implementation

--- graph/Graph.py
+++ graph/Graph.py
@@ -1,3 +1,18 @@
+import math
+
+
+def unvisited_node_with_smallest_distance(not_visited_node: list, distances: dict) -> str:
+    result = not_visited_node[0]  # type: str
+    result_distance = distances[result]  # type: int
+
+    for node in not_visited_node:
+        if distances[node] < result_distance:
+            result = node
+            result_distance = distances[node]
+
+    return result
+
+
 class Graph:
     def __init__(self) -> None:
         self.__vertices = []
@@ -43,3 +58,22 @@ class Graph:
             for edge in self.__adjacency_list[current_node]:
                 neighbor_vertex, weight = edge[0], edge[1]
                 self.dfs_print(visited, neighbor_vertex)
+
+    def dijkstra(self, start: str) -> dict:
+        distances = {vertex: math.inf for vertex in self.__vertices}
+        distances[start] = 0
+
+        not_visited_nodes = [i for i in self.__vertices]
+
+        while len(not_visited_nodes) != 0:
+            current_vertex = unvisited_node_with_smallest_distance(not_visited_nodes, distances)
+            not_visited_nodes.remove(current_vertex)
+
+            for edge in self.__adjacency_list[current_vertex]:
+                neighbor, weight = edge[0], edge[1]
+                current_distance = distances[current_vertex] + weight
+
+                if current_distance < distances[neighbor]:
+                    distances[neighbor] = current_distance
+
+        return distances
